b2c5c57bdd4b2d1938183ed5d4057c005992f8bf
sunjayp888 <sunjayp88@gmail.com>
2017-08-30 15:46:32 +0530

    Document

---
+++ source/Egharpay.Document/DocumentService.cs
@@ -0,0 +1,105 @@
+﻿using Egharpay.Document.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Egharpay.Data;
+using Egharpay.Data.Interfaces;
+
+namespace Egharpay.Document
+{
+    public class DocumentService : IDocumentService
+    {
+        public readonly IPersonnelDataService PersonnelDataService;
+
+        public DocumentService(IPersonnelDataService personnelDataService)
+        {
+            PersonnelDataService = personnelDataService;
+        }
+        public byte[] Bytes(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid Create(int organisationId, int centreId, int categoryId, string studentCode, string studentName, string description, string fileName, byte[] contents)
+        {
+            var newGuid = Guid.NewGuid();
+            var category =
+                PersonnelDataService.RetrieveDocumentTypes(organisationId)
+                    .FirstOrDefault(e => e.DocumentCategoryId == categoryId);
+            var centre = PersonnelDataService.RetrieveCentre(organisationId, centreId, e => true);
+            var basePath = CreateCentreBase(category.BasePath, centre.Name);
+            var categoryFileName = string.Concat(category.Name, "_", newGuid, "_", fileName);
+            var employeeDirectory = GetStudentDirectory(basePath, studentCode) ?? CreateStudentDirectory(basePath, studentName, studentCode);
+            var categoryDirectory = Path.Combine(employeeDirectory, category.Name);
+            var filePath = Path.Combine(categoryDirectory, categoryFileName);
+
+            Directory.CreateDirectory(categoryDirectory);
+            File.WriteAllBytes(filePath, contents);
+            var document = new Entity.Document()
+            {
+                CentreId = centreId,
+                DocumentTypeId = categoryId,
+                StudentCode = studentCode,
+                CreatedDateTime = DateTime.UtcNow.Date,
+                Description = description,
+                FileName = fileName,
+                Location = filePath,
+                StudentName = studentName,
+                Guid = newGuid
+            };
+
+            PersonnelDataService.Create<Entity.Document>(organisationId, document);
+            return newGuid;
+        }
+
+        private static string CreateCentreBase(string basepath, string centreName)
+        {
+            if (!Directory.Exists(Path.Combine(basepath, centreName)))
+                Directory.CreateDirectory(Path.Combine(basepath, centreName));
+            return Path.Combine(basepath, centreName);
+        }
+
+        private static string CreateStudentDirectory(string basePath, string studentName, string studentCode)
+        {
+            var directoryName = Path.Combine(basePath, CleanFilename(String.Format("{0}_{1}", studentName, studentCode)));
+            Directory.CreateDirectory(directoryName);
+            return directoryName;
+        }
+
+        private static string CleanFilename(string filename)
+        {
+            return Path.GetInvalidFileNameChars().Aggregate(filename, (current, chr) => current.Replace(chr, '_')).Replace(' ', '_');
+        }
+
+        private static string GetStudentDirectory(string basePath, string studentCode)
+        {
+
+            var employeeDirectories = Directory.GetDirectories(basePath, String.Format("*_{0}", studentCode));
+            if (!employeeDirectories.Any())
+                return null;
+            if (employeeDirectories.Count() > 1)
+                throw new Exception("Unable to identify employee");
+            return employeeDirectories[0];
+        }
+
+        private static string CreateEmployeeDirectory(string basePath, string employeeName, string payrollId)
+        {
+            var directoryName = Path.Combine(basePath, CleanFilename(String.Format("{0}_{1}", employeeName, payrollId)));
+            Directory.CreateDirectory(directoryName);
+            return directoryName;
+        }
+
+        public byte[] GetDocumentBytes(string path)
+        {
+            return File.ReadAllBytes(path);
+        }
+
+        public List<Entity.Document> RetrieveDocuments(int organisationId, int centreId, string category, string studentCode)
+        {
+            return PersonnelDataService.RetrieveDocuments(organisationId, centreId, category, studentCode).ToList();
+        }
+    }
+}
---
+++ source/Egharpay.Document/Interfaces/IDocumentService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Egharpay.Document.Interfaces
+{
+    public interface IDocumentService
+    {
+        Guid Create(int organisationId, int centreId, int categoryId, string studentCode, string studentName,
+            string description, string fileName, byte[] contents);
+
+        byte[] GetDocumentBytes(string path);
+
+        List<Entity.Document> RetrieveDocuments(int organisationId, int centreId, string category, string studentCode);
+    }
+
+    //public class Document
+    //{
+    //    string Type { get; set; }
+    //    string Filename { get; set; }
+    //    string DocumentCode { get; set; }
+    //    byte[] bytes { get; set; }
+    //    string Location { get; set; }
+    //    string Description { get; set; }
+    //    Guid DocumentGUID { get; set; }
+    //}
+
+    public class DocumentBytes
+    {
+        string FileName { get; set; }
+        byte[] Bytes { get; set; }
+    }
+}
