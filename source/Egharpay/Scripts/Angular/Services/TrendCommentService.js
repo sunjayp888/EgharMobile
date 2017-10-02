7a8203eec5739f715b91db627bda044c4420b86e
vijayraut93 <vijayraut33@gmail.com>
2017-10-02 23:49:01 +0530

    TrendComment Approve

---
+++ source/Egharpay/Scripts/Angular/Services/TrendCommentService.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('Egharpay')
+        .factory('TrendCommentService', TrendCommentService);
+
+    TrendCommentService.$inject = ['$http'];
+
+    function TrendCommentService($http) {
+        var service = {
+            retrieveTrendComments: retrieveTrendComments,
+            approve:approve
+        };
+
+        return service;
+
+        function retrieveTrendComments(Paging, OrderBy) {
+            var url = "/TrendComment/List",
+                data = {
+                    paging: Paging,
+                    orderBy: new Array(OrderBy)
+                };
+            return $http.post(url, data);
+        }
+
+        function approve(trendCommentId) {
+            var url = "/TrendComment/Approve",
+                data = {
+                    trendCommentId: trendCommentId
+                };
+            return $http.post(url, data);
+        }
+    }
+})();
