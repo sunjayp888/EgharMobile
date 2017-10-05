6027ec0667c9d40b130f9533802875fd8890320e
sunjayp888 <sunjayp88@gmail.com>
2017-10-05 08:40:34 +0530

    changes

---
+++ source/Egharpay.Business/Models/MobileImage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egharpay.Business.Models
+{
+    public class MobileImage
+    {
+       public string ImagePath { get; set; }
+    }
+}
