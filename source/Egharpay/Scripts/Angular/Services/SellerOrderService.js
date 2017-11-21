33510f024f974ce8d2937b7172413454a91586a8
paradkarsh24 <paradkarsh24@gmail.com>
2017-11-21 23:22:33 +0530

    SellerOrderGrid

---
+++ source/Egharpay/Scripts/Angular/Services/SellerOrderService.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('Egharpay')
+        .factory('SellerOrderService', SellerOrderService);
+
+    SellerOrderService.$inject = ['$http'];
+
+    function SellerOrderService($http) {
+        var service = {
+            retrieveOrder: retrieveOrder
+            //editOrder: editOrder
+        };
+
+        return service;
+
+        function retrieveOrder(OrderBy) {
+            var url = "/Order/List",
+                data = {
+                    //paging: Paging,
+                    orderBy: new Array(OrderBy)
+                };
+            return $http.post(url, data);
+        }
+
+        }
+})();
