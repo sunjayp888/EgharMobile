7c7b13fd26beb038d420a3dd18ff438f8a84345b
sunjayp888 <sunjayp88@gmail.com>
2018-03-29 19:40:49 +0530

    business service changes

--- source/Egharpay.Business/Services/MobileRepairAdminBusinessService.cs
+++ source/Egharpay.Business/Services/MobileRepairAdminBusinessService.cs
@@ -26,12 +26,12 @@ namespace Egharpay.Business.Services
         public async Task<IEnumerable<AvailableMobileRepairAdmin>> RetrieveAvailableMobileRepairAdmin(DateTime? date, string time)
         {
             var appointmentDateTime = date.CombineDateTime(time);
-            var startTime = appointmentDateTime.Value.AddHours(1);
+            var startTime = appointmentDateTime.Value.AddHours(-1);
             var endTime = appointmentDateTime.Value.AddHours(3);
-            TimeSpan start = new TimeSpan(appointmentDateTime.Value.AddHours(1).Hour, appointmentDateTime.Value.Minute, 0);
+            TimeSpan start = new TimeSpan(appointmentDateTime.Value.AddHours(-1).Hour, appointmentDateTime.Value.Minute, 0);
             TimeSpan end = new TimeSpan(start.Hours + 3, start.Minutes, start.Seconds);
             return await _mobileRepairAdminDataService.RetrieveAsync<AvailableMobileRepairAdmin>
-                (e => !e.AppointmentDate.HasValue || (e.AppointmentDate.HasValue && !e.AppointmentDate.Value.IsBetween(start, end)));
+                (e => !e.AppointmentDate.HasValue || (e.AppointmentDate.HasValue && e.AppointmentDate.Value.TimeOfDay >= start && e.AppointmentDate.Value.TimeOfDay <= end));
 
         }
 
