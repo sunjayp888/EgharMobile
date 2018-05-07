54d41bf35eb6264d3a3d6dc006c54a6448ce79c0
sunjayp888 <sunjayp88@gmail.com>
2018-05-07 19:25:33 +0530

    address Changes

--- source/Egharpay/Scripts/Angular/Controllers/PersonnelController.js
+++ source/Egharpay/Scripts/Angular/Controllers/PersonnelController.js
@@ -5,9 +5,9 @@
         .module('Egharpay')
         .controller('PersonnelController', PersonnelController);
 
-    PersonnelController.$inject = ['$window', 'PersonnelService', 'Paging', 'OrderService', 'OrderBy', 'Order'];
+    PersonnelController.$inject = ['$window', 'PersonnelService', 'AddressService', 'Paging', 'OrderService', 'OrderBy', 'Order'];
 
-    function PersonnelController($window, PersonnelService, Paging, OrderService, OrderBy, Order) {
+    function PersonnelController($window, PersonnelService, AddressService, Paging, OrderService, OrderBy, Order) {
         /* jshint validthis:true */
         var vm = this;
         vm.personnel = [];
@@ -20,6 +20,8 @@
         vm.searchPersonnel = searchPersonnel;
         vm.searchKeyword = "";
         vm.searchMessage = "";
+        vm.openPersonnelShippingAddressModal = openPersonnelShippingAddressModal;
+        vm.createAddress = createAddress;
         initialise();
 
         function initialise() {
@@ -72,5 +74,56 @@
             $window.location.href = "/Personnel/Profile/" + personnelId;
         }
 
+        function openPersonnelShippingAddressModal() {
+            vm.errorMessages = [];
+            vm.fullname = "";
+            vm.email = "";
+            vm.company = "";
+            vm.address1 = "";
+            vm.address2 = "";
+            vm.city = "";
+            vm.landmark = "";
+            vm.pincode = "";
+            vm.state = "";
+            vm.phonenumber = "";
+            vm.district = "";
+            angular.element('#addressModal').modal('toggle');
+        }
+
+        function createAddress() {
+            vm.errorMessages = [];
+            if (!vm.fullname) vm.errorMessages.push('Fullname is required.');
+            if (!vm.address1) vm.errorMessages.push('Address1 is required.');
+            if (!vm.address2) vm.errorMessages.push('Address2 is required.');
+            if (!vm.district) vm.errorMessages.push('District is required.');
+            if (!vm.pincode) vm.errorMessages.push('Pincode is required.');
+            if (!vm.city) vm.errorMessages.push('City is required.');
+            if (vm.errorMessages.length > 0) return;
+
+            var address = {
+                FullName: vm.fullname,
+                Email: vm.email,
+                Company: vm.company,
+                Address1: vm.address1,
+                Address2: vm.address2,
+                City: vm.city,
+                Landmark: vm.landmark,
+                ZipPostalCode: vm.pincode,
+                StateId: 1,
+                PhoneNumber: vm.phonenumber,
+                District: vm.district
+            }
+            return AddressService.createAddress(address)
+                .then(function (response) {
+                    if (response.data === '' || response.data.Succeeded === true) {
+                        vm.canAddNewAddress = false;
+                        retrievePersonnelAddress();
+                    } else {
+                        $('#projectErrorSummary').show();
+                        vm.showErrorSummary = true;
+                        vm.Errors = response.data;
+                    }
+                });
+        }
     }
 })();
