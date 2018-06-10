99c0a6d3121a3d6a558a1c23fc1b37e4a2042f56
sunjayp888 <sunjayp88@gmail.com>
2018-06-10 11:28:58 +0530

    Trend Changes

--- db/UpdateMobileProfileImage.sql
+++ db/UpdateMobileProfileImage.sql
@@ -2,3 +2,14 @@ UPDATE  T1
 SET    T1. ProfileImagePath = '/MobileImage/'+T2.Name+'/'+ T1.Name+'/'+ REPLACE (T1.Name,' ','-')+'.jpg'
 FROM   Mobile as T1 Left JOIN Brand as T2
 ON     T1. BrandId = T2 .BrandId
+where T1.mobileId =12145
+6
+
+
+
+ Select t1.mobileId, '/MobileImage/'+T2.Name+'/'+ T1.Name+'/'+ REPLACE (T1.Name,' ','-')+'.jpg',T1.Name
+FROM   Mobile as T1 Inner JOIN Brand as T2
+ON     T1. BrandId = T2 .BrandId
+where T1.mobileId =12145
+
+select Profile * from mobile where mobileId =12145
